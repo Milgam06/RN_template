52ddb2aa213e29b46034e56cec7880dac91b5177
Milgam06 <milgamfruit@gmail.com>
2024-11-05 15:46:12 +0900

    docs: update to set template

--- src/screens/Main/index.tsx
+++ src/screens/Main/index.tsx
@@ -1,13 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 
-import { Text } from "@/components";
+import { Text } from "@/components/common/Text";
 
 import * as S from "./styled";
 
 export const Main: React.FC = () => (
   <>
     <S.MainScreenContainer>
-      <Text fontSize={400} fontWeight={900}>
+      <Text fontSize={300} fontWeight={300} fontColor="#321412">
         Welcome To Milgam's RN Template!
       </Text>
       <StatusBar style="auto" />
